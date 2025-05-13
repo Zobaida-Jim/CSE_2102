d2e0e3425d2493031de862ce45924a75547d9d2e
Zobaida Jim <z.jimcstucse@gmail.com>
2025-05-13 07:27:46 +0600

    Add files via upload

---
+++ InsertNode_BeginEnd_in_LinkedList.cpp
@@ -0,0 +1,77 @@
+#include<iostream>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* next;
+    Node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+class List{
+    public:
+    Node* head;
+    Node* tail;
+    List()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+    void push_front(int val) //Insert Node At Head
+    {
+        Node* newNode = new Node(val);
+        if(head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void InsertAtEnd(int val) // Insert at end
+    {
+        Node* newNode = new Node(val);
+        Node* temp = head;
+        if(head == NULL)
+        {
+            head = newNode;
+        }
+        else
+        {
+            while(temp->next != NULL)
+            {
+                temp = temp->next;
+            }
+            temp->next = newNode;
+        }
+    }
+    void printList()
+    {
+        Node* temp = head;
+        while(temp != NULL)
+        {
+            cout<<temp->data<<" -> ";
+            temp = temp->next;
+        }
+        cout<<"NULL"<<endl;
+    }
+};
+int main()
+{
+    List LL;
+    LL.push_front(50);
+    LL.push_front(40);
+    LL.push_front(30);
+    LL.push_front(20);
+    LL.push_front(10);
+    LL.printList();
+    LL.InsertAtEnd(64);
+    cout<<"After Insert a Node"<<endl;
+    LL.printList();
+    return 0;
+}
